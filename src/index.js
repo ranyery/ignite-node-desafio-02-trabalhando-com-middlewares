5e63e4bd3449762e3d30c960fa1a8c281ebb2143
ranyery <ranyerysc@gmail.com>
2022-08-20 22:06:59 -0300

    feat: completa funcao do mw findUserById

--- src/index.js
+++ src/index.js
@@ -53,7 +53,13 @@ function checksTodoExists(request, response, next) {
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const user = users.find((user) => user.id === id);
+  if (!user) return response.status(404).send({ error: "User not found!" });
+
+  request["user"] = user;
+
+  next();
 }
 
 app.post("/users", (request, response) => {
